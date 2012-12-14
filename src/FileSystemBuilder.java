4eee4f922d3472e6e7e1c8febc01ff533d9e5f59
jpinheiro <7jpinheiro@gmail.com>
2012-12-14 18:45:22 +0000

    Update FileSystemBuilder.java
    Now creates a N number of N instances to folder ouput

--- src/FileSystemBuilder.java
+++ src/FileSystemBuilder.java
@@ -14,6 +14,7 @@ import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.Err;
 import edu.mit.csail.sdg.alloy4.ErrorWarning;
 import edu.mit.csail.sdg.alloy4compiler.ast.Command;
+import edu.mit.csail.sdg.alloy4compiler.ast.Expr;
 import edu.mit.csail.sdg.alloy4compiler.ast.ExprVar;
 import edu.mit.csail.sdg.alloy4compiler.ast.Module;
 import edu.mit.csail.sdg.alloy4compiler.ast.Sig;
@@ -27,8 +28,21 @@ import edu.mit.csail.sdg.alloy4viz.VizGUI;
 import edu.mit.csail.sdg.alloy4whole.Helper;
 
 
+
+
+
+/**
+ * Builds a filesystem from an alloy instance. 
+ */
 public class FileSystemBuilder {
 
+	/**
+	 * Returns an HashMap object that the key is a string and the value is an object. 
+	 * The iterable argument must be of Iterable Class.
+	 * @param  		iterable  object
+	 * @return      HashMap object that the key is a string and the value is an object
+	 * @see         atom2ObjectMapE
+	 */
 	public static<E> HashMap<String,E> atom2ObjectMapE(Iterable<E> iterable){
 		E e = null;
 		
@@ -41,6 +55,16 @@ public class FileSystemBuilder {
 		return e_Map;
 	}
 	
+	
+	
+	/**
+	 * Returns an object from a Iterable object that has the same name. 
+	 * The iterable argument must be of Iterable Class, and the name a String
+	 * @param  		iterable  object
+	 * @param		String name
+	 * @return      Object that has the same name
+	 * @see         getEFromIterable
+	 */
 	public static <E> E getEFromIterable(Iterable<E> iterable, String name)
 	{
 		E res = null;
@@ -59,7 +83,14 @@ public class FileSystemBuilder {
 	}
 	
 	
-	
+	/**
+	 * Main method that buils a number of filesystem from an alloy
+	 * The iterable argument must be of Iterable Class, and the name a String
+	 * @param  		iterable  object
+	 * @param		String name
+	 * @return      Object that has the same name
+	 * @see         getEFromIterable
+	 */
 	public static void main(String[] args)  throws Err{
 		
 		 A4Reporter rep = new A4Reporter() {
@@ -70,11 +101,11 @@ public class FileSystemBuilder {
 	            }
 	        };
 
-	        for(String filename:args) {
-
-	            // Parse+typecheck the model
-	            System.out.println("=========== Parsing+Typechecking "+filename+" =============");
-	            Module world = CompUtil.parseEverything_fromFile(rep, null, filename);
+	        String filename = args[0];
+	        int test_iterations = Integer.parseInt(args[1]);
+	        // Parse+typecheck the model
+	        System.out.println("=========== Parsing+Typechecking "+filename+" =============");
+	        Module world = CompUtil.parseEverything_fromFile(rep, null, filename);
 	            
 	            /*
 	            SafeList<Sig> sigs = world.getAllSigs();
@@ -89,55 +120,60 @@ public class FileSystemBuilder {
 	            // Choose some default options for how you want to execute the commands
 	            A4Options options = new A4Options();
 	            options.solver = A4Options.SatSolver.SAT4J;
-	            for(Command cmd1: world.getAllCommands()) {
-	            	sol = TranslateAlloyToKodkod.execute_command(rep, world.getAllReachableSigs(), cmd1, options);
-	            	sol =sol.next();
-	            	if (sol.satisfiable()){
-	            		buildFileSystem(sol);
-	            		sol.writeXML("instance.xml");
-	            		new VizGUI(false,"instance.xml",null);
+	            Command cmd1 = world.getAllCommands().get(0);
+	            sol = TranslateAlloyToKodkod.execute_command(rep, world.getAllReachableSigs(), cmd1, options);
+	            for(int i =0; i< test_iterations;i++){
+	            	if (sol.satisfiable())
+	            	{
+	            		buildFileSystem(sol,i);
+	            		sol.writeXML("output/"+i+"/instance"+i+".xml");
 	            	}
-	            }
-	            
+	            	sol=sol.next();
 	            }
 	        }
 	
 	
-	public static void buildFileSystem(A4Solution sol) throws Err
+	public static void buildFileSystem(A4Solution sol,int i) throws Err
 	{
-		
 			Map<String,Sig.PrimSig> mapSigs = Helper.atom2sig(sol);
 			HashMap<String,Sig> sigs = atom2ObjectMapE(sol.getAllReachableSigs());
 			HashMap<String,ExprVar> mapAtoms = atom2ObjectMapE(sol.getAllAtoms());
+			
 			ExprVar state0 = mapAtoms.get("State$0");
-			Sig node = sigs.get("this/Node");
+			Sig nodeSig = sigs.get("this/Node");
 			Sig dir = sigs.get("this/Dir");
+			HashMap<String,Sig.Field> mapNodeFields = atom2ObjectMapE(nodeSig.getFields());
 			Sig.Field root = getEFromIterable(dir.getFields(),"field (this/Dir <: root)");
-			A4TupleSet rootdir = (A4TupleSet) sol.eval(root.join(state0));	
-			Sig.Field parent = getEFromIterable(node.getFields(),"field (this/Node <: parent)");
+			Sig.Field nodeField = mapNodeFields.get("field (this/Node <: node)");
+			//System.out.println(nodeField);
+			A4TupleSet rootdir = (A4TupleSet) sol.eval((root.join(state0)).intersect(nodeField.join(state0)));
+			Sig.Field parent = mapNodeFields.get("field (this/Node <: parent)");
 			A4Tuple tupleroot = rootdir.iterator().next();
 			String path = tupleroot.atom(0).replace('$', '_');
-		
-			Path p = Paths.get(path);
-			try {
-				Files.createDirectory(p);
-				buildTree(sol,path, tupleroot.atom(0), parent,mapAtoms,mapSigs);
-			}catch (IOException e) { System.out.println("IOException, provavelmente directoria já existe");}
+			
+			Expr parents = nodeField.join(state0).domain(parent);
+	
+			path = "output/"+i+"/"+path;
+			Path p = Paths.get( path);
+			try {				
+				Files.createDirectories(p);
+				buildTree(sol,path, tupleroot.atom(0), parents,mapAtoms,mapSigs);
+			}catch (IOException e) { System.out.println("IOException, Directory already exists!\n");}
 		}
 	
-	private static void buildTree(A4Solution sol,String path, String atom, Sig.Field parent,HashMap<String,ExprVar> mapAtom,Map<String,Sig.PrimSig> mapSig) throws Err, IOException
+	private static void buildTree(A4Solution sol,String path, String atom, Expr parent,HashMap<String,ExprVar> mapAtom,Map<String,Sig.PrimSig> mapSig) throws Err, IOException
 	{
 		ExprVar current = mapAtom.get(atom);
 
-		A4TupleSet filhos = (A4TupleSet) sol.eval(parent.join(current));
-		for(A4Tuple filho : filhos)
+		A4TupleSet children = (A4TupleSet) sol.eval(parent.join(current));
+		for(A4Tuple child : children)
 		{
-			String newpath = path +"/" +filho.atom(0).replace('$', '_');
+			String newpath = path +"/" +child.atom(0).replace('$', '_');
 			Path p = Paths.get(newpath);
-			if(mapSig.get(filho.atom(0)).toString().equals("this/Dir"))
+			if(mapSig.get(child.atom(0)).toString().equals("this/Dir"))
 			{
 				Files.createDirectory(p);
-				buildTree(sol,newpath,filho.atom(0),parent,mapAtom,mapSig);
+				buildTree(sol,newpath,child.atom(0),parent,mapAtom,mapSig);
 			}
 			else 
 				Files.createFile(p);
