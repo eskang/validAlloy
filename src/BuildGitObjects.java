a312f45a3f052ee619cf86a17b3b5c336ab6b8bb
7jpinheiro <7jpinheiro@gmail.com>
2013-01-17 18:02:07 +0000

    BuildTreeEntry and BuildGitTree Added

--- src/BuildGitObjects.java
+++ src/BuildGitObjects.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 
 import edu.mit.csail.sdg.alloy4.Err;
@@ -102,6 +103,73 @@ public class BuildGitObjects {
 	
 	}
 
+	
+	public static String buildTreeEntry(String mode,String type,String hashcode,String desc){
+		
+		String entry = mode + " " + type + " " + hashcode + "\t" + desc + "\n";
+		
+		return entry;
+		
+	}
+	
+	
+	public static String buildGitTree(ArrayList<String> entrys, String pathindex){
+	
+		String hashcode = null;
+	
+		try{
+			
+			StringBuilder tree = new StringBuilder();
+			
+			for(String line : entrys){
+				tree.append(line);
+			}
+			 
+			System.out.println(tree.toString());
+			 
+			String newpath = "output/"+pathindex;
+			 
+			File path = new File(newpath);
+			
+			ProcessBuilder pb = new ProcessBuilder("git","mktree");
+			
+			pb.directory(path);	
+			
+			Process pr = pb.start();
+			
+		
+			
+			OutputStream out = pr.getOutputStream();
+			InputStream in = pr.getInputStream();
+			InputStream err = pr.getErrorStream();
+
+			InputStreamReader isr = new InputStreamReader(in);
+			OutputStreamWriter osr = new OutputStreamWriter(out);
+			
+			
+			BufferedReader br = new BufferedReader(isr);
+			BufferedWriter bw = new BufferedWriter(osr);
+			
+			bw.flush();
+			bw.write(tree.toString());
+			bw.close();
+		
+		
+			hashcode = br.readLine();
+			System.out.println(hashcode);
+			
+			br.close();
+			pr.destroy();
+		
+		
+		}catch(Exception exc){
+			exc.printStackTrace();
+		}
+		return hashcode;
+		
+	}
+
+	
 	public static void buildObjects(A4Solution sol,Module world, String index) throws Err
 	{
 		Expr e = CompUtil.parseOneExpression_fromString(world, "Blob <: object.State");
