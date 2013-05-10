000a5405e315583b95cfdae470e12f9377c6b05b
7jpinheiro <7jpinheiro@gmail.com>
2013-05-10 02:50:53 +0100

    Add GitException.java

---
+++ src/GitException.java
@@ -0,0 +1,6 @@
+public class GitException extends Exception {
+  public GitException() { super(); }
+  public GitException(String message) { super(message); }
+  public GitException(String message, Throwable cause) { super(message, cause); }
+  public GitException(Throwable cause) { super(cause); }
+}
